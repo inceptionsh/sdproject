4dcf2056f103905a00361f926be63989d9e333ec
inceptionsh <142550910+inceptionsh@users.noreply.github.com>
2024-03-11 12:47:31 +0600

    Add files via upload

---
+++ MainExplorerApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mainexplorerapp;
+
+/**
+ *
+ * @author Hp
+ */
+public class MainExplorerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
