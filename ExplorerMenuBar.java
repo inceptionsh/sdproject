fd90a679f9bd531b594105fac757681a7d17a766
inceptionsh <142550910+inceptionsh@users.noreply.github.com>
2024-03-11 13:40:07 +0600

    Add files via upload

--- ExplorerMenuBar.java
+++ ExplorerMenuBar.java
@@ -13,8 +13,10 @@ public class ExplorerMenuBar extends JMenuBar {
        
 
         JMenu fileMenu = new JMenu("File");
-        
-        
+        JMenu sort = new JMenu("sort");
+        JMenu view = new JMenu("view");
+        JMenu Mode = new JMenu("Mode");
+        JMenu Drive = new JMenu("Drive");
         JMenu Edit = new JMenu("edit");
 
         JMenuItem createFileItem = new JMenuItem("Create File");
@@ -45,7 +47,49 @@ public class ExplorerMenuBar extends JMenuBar {
                 System.exit(0);
             }
         });
-  
+        JMenuItem Ascending = new JMenuItem("Ascending");
+        Ascending.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               FileOperations.sortFilesAscending();
+            }
+        });
+
+        JMenuItem Descending = new JMenuItem("Descending");
+        Descending.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FileOperations.sortFilesDescending();
+            }
+        });
+        JMenuItem list= new JMenuItem("list");
+        list.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              //  FileOperations.listview(); 
+            }
+        });
+        JMenuItem grid= new JMenuItem("grid");
+        grid.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               // FileOperations.gridview(); 
+            }
+        });
+        JMenuItem Day= new JMenuItem("Day");
+        Day.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             
+            }
+        });
+        JMenuItem Night= new JMenuItem("Night");
+        Night.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             // FileOperations.panelbackground(); 
+            }
+        });
         exitItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -53,7 +97,23 @@ public class ExplorerMenuBar extends JMenuBar {
             }
         });
        
-       
+         JMenuItem CDrive = new JMenuItem("CDrive");
+         CDrive.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                
+            }
+        });
+         
+         
+          JMenuItem DDrive = new JMenuItem("DDrive");
+         DDrive.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
           //implements edit menu
            JMenuItem copy = new JMenuItem("copy");
          copy.addActionListener(new ActionListener() {
@@ -89,13 +149,26 @@ public class ExplorerMenuBar extends JMenuBar {
         Edit.add(copy);
         Edit.add(cut);
         Edit.add(paste);
+       
+        sort.add(Ascending);
+        sort.add(Descending);
+        view.add(list);
+        view.add(grid);
+        Mode.add(Day);
+        Mode.add(Night);
+        Drive.add(CDrive);
+        Drive.add(DDrive);
 
         
         
 
         add(fileMenu);
-        
+        add(sort);
+        add(view);
         add(Edit);
-       
+        add(Mode);
+        
+        add(Drive);
+        
     }
 }
